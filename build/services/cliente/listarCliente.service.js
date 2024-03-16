552970b38a4a2592e0401109140129fe3fb24274
ElyMuniz <elymuzz@hotmail.com>
2024-03-16 12:34:13 -0300

    voltei a versão do node

--- build/services/cliente/listarCliente.service.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-const prisma = new client_1.PrismaClient();
-const listarClienteService = async () => {
-    const clientes = await prisma.cliente.findMany({});
-    return clientes;
-};
-exports.default = listarClienteService;
